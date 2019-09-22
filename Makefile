e51fa605c012957b1c62a7a7f69b8cb3c227e0bb
Ignacio Torres Masdeu <i@itorres.net>
2019-09-23 00:33:50 +0200

    Add top quoters and some error handling

---
+++ Makefile
@@ -0,0 +1,4 @@
+sync: diosteama
+	scp diosteama ubuntu@172.23.0.154:
+diosteama:
+	go build
--- diosteama.go
+++ diosteama.go
@@ -15,6 +15,7 @@ import (
 
 var db *sql.DB
 var loc *time.Location
+
 func main() {
 	var err error
 	token := os.Getenv("TELEGRAM_BOT_TOKEN")
@@ -50,17 +51,40 @@ func main() {
 			if len(split) == 2 {
 				q = split[1]
 			}
-			msg = tgbotapi.NewMessage(update.Message.Chat.ID, quote(q))
+			reply, err := quote(q)
+			if err != nil {
+				log.Println("Error reading quote: ", err)
+			}
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+		} else if split[0] == "!top" || split[0] == "/top" {
+			var i int
+			if len(split) == 2 {
+				var err error
+				i, err = strconv.Atoi(split[1])
+				if err != nil {
+					i = 10
+				}
+			} else {
+				i = 10
+			}
+			r, err := top(i)
+			if err != nil {
+				log.Println("Error reading top", err)
+			}
+			reply := strings.Join([]string{"```", r, "```"}, "")
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+			msg.ParseMode = "markdown"
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
 		} else {
 			log.Printf("[%s] %s (%v)", update.Message.From.UserName, update.Message.Text, update.Message.IsCommand())
 		}
-
-		/* 		msg.ReplyToMessageID = update.Message.MessageID */
-		bot.Send(msg)
 	}
 }
 
-func quote(q string) string {
+func quote(q string) (string, error) {
 	var (
 		recnum              int
 		date, author, quote string
@@ -80,17 +104,54 @@ func quote(q string) string {
 	err := db.QueryRow(fmt.Sprintf("%s %s", query, f)).Scan(&recnum, &quote, &author, &date)
 
 	if err != nil {
-		return("Quote no encontrado")
+		return "Quote no encontrado", err
 	}
 	log.Println(recnum, quote, author, date)
 	split := strings.SplitN(author, "!", 2)
-	return fmt.Sprintf("%s\n\n-- Quote %d by %s on %s", quote, recnum, split[0], parseTime(date))
+	nick := split[0]
+	//💩🔞🔪💥
+	return fmt.Sprintf("%s\n\n🚽 Quote %d by %s on %s", quote, recnum, nick, parseTime(date)), nil
+}
+
+func top(i int) (string, error) {
+	var b strings.Builder
+	var err error
+	if i < 0 {
+		i = 10
+	}
+	rows, err := db.Query("select count(*) as c, substring_index(author, '!', 1) as a from linux_gey_db group by a order by c desc limit ?;", i)
+	if err != nil {
+		log.Printf("Error listing top %d. Fuck you.", i)
+		return b.String(), err
+	}
+	defer rows.Close()
+	i = 0
+	for rows.Next() {
+		i++
+		var (
+			count  int
+			author string
+		)
+		err := rows.Scan(&count, &author)
+		if err != nil {
+			log.Printf("Error scanning top results. Fuck you all!")
+			return b.String(), err
+		}
+		log.Println(count, author)
+		fmt.Fprintf(&b, "%3d %20s %5d\n", i, author, count)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Printf("Error in the final possible place in the top 10. Fuck you all! And especially you!")
+		return b.String(), err
+	}
+	return b.String(), err
 }
 
 func parseTime(t string) time.Time {
 	i, err := strconv.ParseInt(t, 10, 64)
 	if err != nil {
-		panic(err)
+		i = 1
 	}
 	tm := time.Unix(i, 0).In(loc)
 	return tm
