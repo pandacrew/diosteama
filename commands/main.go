0f602bb2d02a6e5a970dd22bf3186b364aba22e6
Ambatlle <jaumea@gmail.com>
2021-04-24 13:40:43 +0200

    Refactor to make commands invokation less intrusive and a little bit generic

--- commands/main.go
+++ commands/main.go
@@ -6,35 +6,28 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+type commandFunction func(tgbotapi.Update, *tgbotapi.BotAPI, []string)
+
+var invokers = map[string]commandFunction{
+	"addquote": addquoteStart,
+	"quote":    quote,
+	"info":     info,
+	"rquote":   rquote,
+	"top":      top,
+	"culote":   culote,
+	"chuches":  chuches,
+	"w00g":     w00g,
+	"soy":      soy,
+	"quienes":  quienes,
+	"es":       es,
+}
+
 // Command executes a bot command
 func Command(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
 	argv := strings.SplitN(update.Message.Text, " ", 3)
 	cmd := argv[0][1:]
 	args := argv[1:]
 
-	switch cmd {
-	case "addquote":
-		addquoteStart(update, bot, args)
-	case "quote":
-		quote(update, bot, args)
-	case "info":
-		info(update, bot, args)
-	case "rquote":
-		rquote(update, bot, args)
-	case "top":
-		top(update, bot, args)
-	case "culote":
-		culote(update, bot, args)
-	case "chuches":
-		chuches(update, bot, args)
-	case "w00g":
-		w00g(update, bot, args)
-	case "soy":
-		soy(update, bot, args)
-	case "quienes":
-		quienes(update, bot, args)
-	case "es":
-		es(update, bot, args)
-
-	}
+	fn := invokers[cmd]
+	fn(update, bot, args)
 }
